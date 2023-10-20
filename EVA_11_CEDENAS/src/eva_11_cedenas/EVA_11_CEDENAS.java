dd65af865f4d88185d841010d357c43810ab9caa
BrunoBucciaratijojos <143563080+BrunoBucciaratijojos@users.noreply.github.com>
2023-10-20 15:04:57 -0600

    Add files via upload

---
+++ EVA_11_CEDENAS/src/eva_11_cedenas/EVA_11_CEDENAS.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva_11_cedenas;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA_11_CEDENAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String cade = "hola";
+        
+        if (cade == hola) {
+            
+            System.out.println("las cadenas son iguales");
+            
+        }else{
+            System.out.println("las cadenas no son iguales ");
+        
+        }
+        
+        
+        
+    }
+    
+}
